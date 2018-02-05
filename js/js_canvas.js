58234ae66d809eaae2e572150792a138613a063e
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-02-05 07:10:41 -0500

    u

--- js/js_canvas.js
+++ js/js_canvas.js
@@ -1,10 +1,11 @@
    // ---//
 
-   var vw = $("views").width();
-   var vh = $("views").height();
+   var vw = $("video").width();
+   var vh = $("video").height();
 
    var vidW = $("video").width();
    var vidH = $("video").height();
+
    var vidProp = vidW / vidH;
    var vProp = vw / vh;
 
@@ -34,21 +35,31 @@
 
        setInterval(function () {
 
-           vw = $(document).width();
-           vh = $(document).height();
+           vw = $("video").width();
+           vh = $("video").height();
 
            vidW = $("video").width();
            vidH = $("video").height();
+           
            vidProp = vidW / vidH;
            vProp = vw / vh;
+           
+           sx = 0; sy = 0;
+           sw = 0; sh = 0;
+           dx = vw; dy = vh;
+           dw = vw ;dh = vh;
+           
+        tsContextOut.drawImage(tsVideo,0,0,100,100);
+           
+           
+/*
+           tsContextFx1.drawImage(tsCanvasIn,0,0,sx,sy,sw,sh,dx, dy, dw,dh)
 
-           tsContextIn.drawImage(tsVideo, 0, 0, vw, vw / vidProp);
-
-           tsContextFx1.drawImage(tsCanvasIn, 0, 0);
            tsCanvasFx1 = applyFx1(CurrentFx, tsCanvasIn);
            tsContextFx1 = tsCanvasFx1.getContext('2d');
 
-           tsContextFx2.drawImage(tsCanvasIn, 0, 0);
+           tsContextFx2.drawImage(tsCanvasIn,0,0,sx,sy,sw,sh,dx, dy, dw,dh)
+
            tsCanvasFx2 = applyFx2(CurrentFx, tsCanvasIn);
            tsContextFx2 = tsCanvasFx2.getContext('2d');
 
@@ -58,28 +69,23 @@
            tsContextFx2Masked = tsCanvasFx2Masked.getContext('2d');
 
 
+
+
            tsCanvasMerged = merged(tsCanvasFx1, tsCanvasFx2Masked);
            tsContextMerged = tsCanvasMerged.getContext('2d');
 
 
-           tsContextOut.drawImage(tsCanvasMerged, 0, 0);
+
+           tsContextOut.drawImage(tsCanvasMerged,0,0,sx,sy,sw,sh,dx, dy, dw,dh)
 
 
+*/
 
        }, 0);
 
 
 
    }
-
-
-
-
-
-
-
-
-
 
 
 
@@ -98,9 +104,23 @@
    }
 
    function masked(tsCanvas) {
+
+
        return tsCanvas
    }
 
    function merged(tsCanvas) {
-       return tsCanvas
+/*
+       var ctx = tsCanvas.getContext("2d");
+       var data = ctx.getImageData(0, 0, vw, vh);
+
+       for (var i = 0, length = data.data.length; i < length; i += 4) {
+           data.data[i] = Math.max(255, data.data[i]);
+       }
+
+       tsContextMerged.putImageData(data, 0, 0);
+  */
+       
+  return tsCanvas
+
    }
